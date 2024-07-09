65dc8d5fc65d0d9b22bffadcf1bd879ebae90999
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-07-09 17:36:49 +0500

    Changed the tab name for the Profile page

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,28 +1,10 @@
 import ProfileView from "@/components/Dashboard/ProfileView";
 import { cookies } from 'next/headers'
 
-export async function generateMetadata(props: any) {
-  const { id } = props.params;
-  const cookieStore = cookies()
-  const token = cookieStore.get('token')
-
-  const response = await fetch(`https://api.sedaems.originsmartcontrols.com/v1/users/me`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token?.value}`
-    }
-  });
-
-  const data = await response.json();
-
-  const { firstName, lastName } = data.user
-  const title = `${firstName} ${lastName} - Profile`;
-
-  return {
-    title: title,
-  };
+export const metadata = {
+  title: 'Profile'
 }
+
 const ProfilePage = () => {
   return <ProfileView />;
 };
